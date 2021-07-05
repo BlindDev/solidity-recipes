16d21e34fbcab58b0d108e5692dd5d4e9a1daa04
Pavel Popov <lonelyrealist@gmail.com>
2021-07-05 21:39:40 +0300

    more secure random

--- Book/Examples/Random.sol
+++ Book/Examples/Random.sol
@@ -1,8 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.6.12 <0.9.0;
 
-contract Random {
-  function generate(uint mod) external view returns(uint){
-    return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % mod;
+contract Oracle {
+  address admin;
+  uint public rand;
+
+  constructor() {
+    admin = msg.sender;
+  }
+
+  function feedRandomness(uint _rand) external {
+    require(msg.sender == admin);
+    rand = _rand;
+  }
+}
+contract MyContract {
+
+  Oracle oracle;
+  uint nonce;
+  constructor(address _oracleAddress){
+    oracle = Oracle(_oracleAddress);
+  }
+  function generate(uint mod) internal returns(uint){
+    uint rand = uint(keccak256(abi.encodePacked(nonce,oracle.rand,block.timestamp, block.difficulty, msg.sender))) % mod;
+    nonce++;
+
+    return rand;
   }
 }
